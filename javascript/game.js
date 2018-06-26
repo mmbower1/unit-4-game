be0b71da0c0e58c8d37b12f3f8d0c26ed68ef555
Matthew Bower <mttbwr91@gmail.com>
2018-06-26 00:29:32 -0700

    star wars psuedo coded

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,41 @@
+var player1;
+var attacker;
+var defender = false;
+
+//each character has three attributes: healthPoints, attackPower, counterAttackPower
+
+$(document).ready(function() {
+
 //create onClick event for each character
 $('.character').on('click', function() {
-    alert($(this).attr("data"));
-    $('#yourCharacterRow').append($(this))
+    alert($(this).attr('data'));
+    $('#yourCharacterRow').append($(this));
 })
+
+//enemies move to defender area when clicked
+$('.character').on('click', function() {
+    alert($(this).attr('data'));
+    $('#defender-row').append($(this));
+})
+
+//player now is able to click 'attack' button. Defender loses hp. Points are displayed at bottom 
+
+//defender is attacked and instantly counter-attacks, player loses hp, points displayed at players section
+
+//after each defender is defeated click on next one to attack next
+
+//player wins by defeating all defenders hp, player loses if hp goes to 0 or below
+
+//game restart 
+
+//each time the player attacks, their character's Attack Power increases by its base Attack Power.
+//EX: base = (6) + 6 = 12 + 6 = 18 + 6 = 24 + 6 = 30
+
+//the defender only has one constant counter attack power
+
+//the Health Points, Attack Power and Counter Attack Power of each character must differ.
+
+});
+
+
+
